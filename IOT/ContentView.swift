484cb197d3b5c52951b6a2cdab56176e7ee3f8fd
Ben Howarth <benjamin.howarth@sjsu.edu>
2020-11-27 14:21:50 -0800

    Added ON/OFF button

--- IOT/ContentView.swift
+++ IOT/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                VStack {
+                    Text("LED Light Control")
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                            .padding()
+                }
+                HStack {
+                    Button(action: {}) {
+                        Text("On")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.red)
+                    }
+                    .padding(.all)
+                    .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    Button(action: {}) {
+                        Text("Off")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.red)
+                    }
+                    .padding(.all)
+                    .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
+}
 }
