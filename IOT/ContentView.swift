ab198cdaffe5238a66f1a80c486029c9496b67b1
Ben Howarth <benjamin.howarth@sjsu.edu>
2020-11-27 15:58:02 -0800

    Added LED color Object, Changed the text to show LED state.

--- IOT/ContentView.swift
+++ IOT/ContentView.swift
@@ -7,27 +7,61 @@
 
 import SwiftUI
 
+struct LEDColor {
+    private var color: Int = -1
+
+    func getColorText() -> String {
+        if self.color <= 0 {
+            return "OFF"
+        } else {
+            return "ON"
+        }
+    }
+    
+    mutating func setColor(c: Int) {
+        self.color = c
+    }
+
+    init() {
+        self.color = -1
+    }
+}
+
 struct ContentView: View {
+    
+    @State var color = LEDColor()
+    
     var body: some View {
         VStack {
             VStack {
-                VStack {
-                    Text("LED Light Control")
+                HStack {
+                    Text("LED Light Controller ")
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                            .padding()
+                    Text(self.color.getColorText())
                         .font(.title2)
                         .fontWeight(.black)
                         .foregroundColor(Color.white)
                             .padding()
                 }
                 HStack {
-                    Button(action: {}) {
-                        Text("On")
+                    Button(action: {
+                        print("ON Button Pressed!")
+                        self.color.setColor(c: 255)
+                    }) {
+                        Text("ON")
                             .fontWeight(.semibold)
                             .foregroundColor(Color.red)
                     }
                     .padding(.all)
                     .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                    Button(action: {}) {
-                        Text("Off")
+                    Button(action: {
+                        print("OFF Button Pressed!")
+                        self.color.setColor(c: -1)
+                    }) {
+                        Text("OFF")
                             .fontWeight(.semibold)
                             .foregroundColor(Color.red)
                     }
@@ -43,8 +77,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-                .preferredColorScheme(.dark)
-        }
+            .preferredColorScheme(.dark)        }
     }
 }
 }
