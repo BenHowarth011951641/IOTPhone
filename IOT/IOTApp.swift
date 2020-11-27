64418de3e4a73e6a7b32a329280088beed1c0ab7
Ben Howarth <benjamin.howarth@sjsu.edu>
2020-11-27 13:14:15 -0800

    Hello World!

---
+++ IOT/IOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOTApp.swift
+//  IOT
+//
+//  Created by Benjamin Howarth on 11/27/20.
+//
+
+import SwiftUI
+
+@main
+struct IOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
